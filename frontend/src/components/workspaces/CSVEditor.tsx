e4d25ad20871b8ee3942333f718b56eee74c7489
Michael Mills <mmills6060@gmail.com>
2025-08-28 11:43:15 -0400

    set outside click back to false

--- frontend/src/components/workspaces/CSVEditor.tsx
+++ frontend/src/components/workspaces/CSVEditor.tsx
@@ -2617,7 +2617,7 @@ const CSVEditor: React.FC<CSVEditorProps> = ({
             licenseKey="non-commercial-and-evaluation"
             manualRowResize={true}
             manualColumnResize={true}
-            outsideClickDeselects={true}
+            outsideClickDeselects={false}
             selectionMode="multiple"
             afterChange={handleDataChange}
             afterSelectionEnd={(r: number, c: number, r2: number, c2: number) => { lastSelectionRef.current = [r,c,r2,c2]; }}
