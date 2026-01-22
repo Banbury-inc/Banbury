ebbb72bdae89fe7f8880ac7fb4ae5daccec85a96
Michael Mills <mmills6060@gmail.com>
2026-01-22 17:11:37 -0500

    fix build errors

--- packages/pages/admin.tsx
+++ packages/pages/admin.tsx
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic';
 
 // Use suspense to avoid aborted fetch during rapid route transitions
-const Admin = dynamic(() => import('frontend/pages/Admin/Admin'), { ssr: false, suspense: true });
+const Admin = dynamic(() => import('frontend/pages/admin'), { ssr: false, suspense: true });
 
 export default function AdminPage() {
   return (
--- packages/pages/news/index.tsx
+++ packages/pages/news/index.tsx
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic';
 const Layout = dynamic(() => import('frontend/layout/Layout'), { ssr: false });
-const News = dynamic(() => import('frontend/components/News'), { ssr: false });
+const News = dynamic(() => import('frontend/pages/News'), { ssr: false });
 
 export default function NewsPage() {
   return (
